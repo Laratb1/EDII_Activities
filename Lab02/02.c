cf295d839666b45c89cfcfaca39ec73129e278cf
Laratb1 <lara.tagarro@gmail.com>
2022-09-25 19:16:37 -0300

    Teste

--- Lab02/02.c
+++ Lab02/02.c
@@ -6,22 +6,22 @@
 int main(){
     srand(time(NULL)); // should only be called once
     int r; // returns a pseudo-random integer between 0 and RAND_MAX
-    
-    Bst *treeLeft = createEmptyTree();
-    Bst *treeRight = createEmptyTree();
-    Bst *tree = createTree(3, NULL, NULL);
-
     int N;
+
+    printf("Digite a quantidade de elementos da arvore: ");
     scanf("%d", &N);
 
-    r = rand();
-    createTree(r, createEmptyTree(), createEmptyTree());
-    for(int i = 0; i <= N; i++){
+    Bst *tree = createEmptyTree();
+
+    for(int i = 0; i < N; i++){
         r = rand();
         tree = insert(r, tree);
     }
 
     printTree(tree);
+    printf("\nAltura: %d", height(tree));
+
+    freeTree(tree);
 
     return 0;
 }
